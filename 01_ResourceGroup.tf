873df8c52306b2534b3f4662b101b0ec0b2101ca
sudhvihaan <m.k.sudhakar@gmail.com>
2023-10-31 15:52:18 +0000

    Delete 01_ResourceGroup.tf

--- 01_ResourceGroup.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "RG" {
-  name     = var.azurerm_rg_name  # Just use the variable directly
-  location = var.azurerm_rg_location  # Just use the variable directly
-}
-
