78ee4c15805033852382d311ec2b03b54e6f9e3d
sudhvihaan <m.k.sudhakar@gmail.com>
2023-11-05 22:59:14 +0000

    Update providers.tf

--- 04-CICD-PL-Java-SpringBootApp-AzureApps/Terraform/providers.tf
+++ 04-CICD-PL-Java-SpringBootApp-AzureApps/Terraform/providers.tf
@@ -1,27 +1,21 @@
+
+
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.44.1"
+      version = ">= 3.15.00"
     }
   }
-
-/*
-  # Backend configuration for storing state in Azure Storage
   backend "azurerm" {
-  resource_group_name   = "RG_DP_ACA"  
-    resource_group_name   = "AKS-HEMLDemo"
-    storage_account_name  = "storageac6674" # Replace with your Azure Storage account name
-    container_name        = "storestafefile"       # Replace with the container name within the Storage account
-    key                   = "terraformd-dev.tfstate"        # The name of the state file within the container
+    resource_group_name = "RG_JAVA_APP"
+    storage_account_name = "storageac6674"
+    container_name = "storestafefile"
+    key = "statefile.tfstate"
   }
-}*/
-
-
-
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
 }
