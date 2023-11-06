814a74c463fe516462f483f68b571136b3f64e5d
sudhvihaan <m.k.sudhakar@gmail.com>
2023-11-06 23:01:06 +0000

    Update 03_App_Service_Creation.tf

--- 04-CICD-PL-Java-SpringBootApp-AzureApps/Terraform/03_App_Service_Creation.tf
+++ 04-CICD-PL-Java-SpringBootApp-AzureApps/Terraform/03_App_Service_Creation.tf
@@ -19,7 +19,7 @@ resource "azurerm_app_service" "app_service" {
 
   site_config {
    // always_on        = false
-    java_version           = "8"
+    java_version           = "11"
     java_container         = "TOMCAT"
     java_container_version = "10"
   }
